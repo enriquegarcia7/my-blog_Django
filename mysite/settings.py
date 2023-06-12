0ac764099d0376b17207f7b7fd83b67febe56434
enriquegarcia7 <enrique7729@gmail.com>
2023-06-12 09:08:55 -0400

    debug correguido

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ozqk57mbv@mko0mwe7o-x7p=7bj#rqve)w3)-jw5si_+q#0bu*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'enrique.pythonanywhere.com']
 
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATC_ROOT = BASE_DIR /'static'
+STATIC_ROOT = BASE_DIR /'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
